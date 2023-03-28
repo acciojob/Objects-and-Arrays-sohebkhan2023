35a2969fa0624f0e1221499e164b0dab6c5c893a
Sohebkhan <102939844+sohebkhan2023@users.noreply.github.com>
2023-03-28 14:03:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 var team == players;
-var team1 == players
+var team1 == players;
 var cap1 == person;
 
 export {players,person,team,team1,cap1}
